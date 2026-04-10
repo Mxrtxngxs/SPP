d8e774ca7b646ada5fd06b69c8b83d9bb59d91e4
Mxrtxn <martin5133h@gmail.com>
2026-04-10 01:36:56 -0600

    Creacion de CoordinatorDAOImplementation

--- src/main/java/mx/uv/spp/business/dto/CoordinatorDetailDTO.java
+++ src/main/java/mx/uv/spp/business/dto/CoordinatorDetailDTO.java
@@ -1,6 +1,14 @@
 package mx.uv.spp.business.dto;
 
-public class CoordinatorDetailDTO {
+public class CoordinatorDetailDTO extends UserDTO {
     private Integer userId;
     private String staffNumber;
+
+    public String getStaffNumber() {
+        return staffNumber;
+    }
+
+    public void setStaffNumber(String staffNumber) {
+        this.staffNumber = staffNumber;
+    }
 }
